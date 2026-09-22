b4d5716eea667dab84921ca6a95a9b502bcb1dd6
tahseensyl-coder <tahseen.syl@gmail.com>
2026-09-22 14:13:29 -0400

    Create Arithmetic.py

---
+++ Arithmetic.py
@@ -0,0 +1,33 @@
+# Arithmetic
+
+# y = (m * x) + b
+m = 5.0
+x = 2
+b = 25
+
+# y = (m *x) + b
+print(0) 
+
+
+# String Manipulation 
+first_name = 'tahseen'
+last_name = 'Rahman'
+
+print(f'{first_name[7:].replace('tahseen', 'Tahseen') }')
+
+
+# try/Except - Casting/Conversion
+try: 
+    person_weight = input("Enter your weight now!: ")
+except: 
+    print("You're Wrong!")
+    
+     
+
+    
+
+
+    
+
+
+
